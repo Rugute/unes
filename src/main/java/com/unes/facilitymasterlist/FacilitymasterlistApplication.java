924e5ce44bc26ec7d1f0e3bb80e63fc7a306fede
Rugute <rrugute@gmail.com>
2021-03-06 20:03:49 +0300

    added packages

---
+++ src/main/java/com/unes/facilitymasterlist/FacilitymasterlistApplication.java
@@ -0,0 +1,13 @@
+package com.unes.facilitymasterlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilitymasterlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilitymasterlistApplication.class, args);
+	}
+
+}
